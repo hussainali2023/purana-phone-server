022093f32e4dada22401721c80827f3b7d44e73c
hussain2023 <iamhussainali2@gmail.com>
2022-11-24 18:26:36 +0530

    send users data to server when register

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cors());
-app.use(express());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xgu9cba.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const usersCollection = client.db("puranaPhone").collection("users");
+
+    app.post("/adduser", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+  } catch (data) {
+    console.log(data);
+  }
+}
+run().catch((data) => console.log(data));
 
 app.get("/", async (req, res) => {
   res.send("Server is Running");
